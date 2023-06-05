b8e23889d2b981e0199ddfcec5274600f026427b
canknbr <cankanbur1@gmail.com>
2023-06-05 18:10:24 +0300

    Initial Commit

---
+++ Blogggy/BlogggyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlogggyApp.swift
+//  Blogggy
+//
+//  Created by Can Kanbur on 5.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct BlogggyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
